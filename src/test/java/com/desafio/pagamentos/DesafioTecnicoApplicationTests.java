975fa6789bfd8b8773a82ffc1caf4df516afc7df
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-09-23 15:43:34 -0300

    Inicio de projeto

---
+++ src/test/java/com/desafio/pagamentos/DesafioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
